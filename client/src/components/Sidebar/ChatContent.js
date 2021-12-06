ca60dd73568e6bdd2ccbacdbb0532fa941335008
michael-levitski <levitski.michael@gmail.com>
2021-12-06 01:03:17 -0800

    add unread count UI and unread bolding

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Chip } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
+const previewTextStyle = {
+  fontSize: 12,
+  color: "#9CADC8",
+  letterSpacing: -0.17,
+}
+
+const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     justifyContent: "space-between",
@@ -13,18 +19,25 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: "bold",
     letterSpacing: -0.2,
   },
-  previewText: {
-    fontSize: 12,
-    color: "#9CADC8",
-    letterSpacing: -0.17,
+  unreadPreviewText: {
+    ...previewTextStyle,
+    color: "black",
+    fontWeight: "bold"
   },
+  readPreviewText: {
+    ...previewTextStyle
+  },
+  unreadMessageCount: {
+    marginRight: 20,
+    alignSelf: "center"
+  }
 }));
 
 const ChatContent = (props) => {
   const classes = useStyles();
 
   const { conversation } = props;
-  const { latestMessageText, otherUser } = conversation;
+  const { latestMessageText, otherUser, currentUserUnreadCount } = conversation;
 
   return (
     <Box className={classes.root}>
@@ -32,10 +45,17 @@ const ChatContent = (props) => {
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
+        <Typography className={currentUserUnreadCount ? classes.unreadPreviewText: classes.readPreviewText}>
           {latestMessageText}
         </Typography>
       </Box>
+      {!!currentUserUnreadCount &&
+        <Chip
+          className={classes.unreadMessageCount}
+          label={currentUserUnreadCount}
+          color="primary"
+          size="small"
+        />}
     </Box>
   );
 };
