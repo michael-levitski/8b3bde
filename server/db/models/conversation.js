87158542376dbbee4af9e53505c9dadb8e5c0523
michael-levitski <levitski.michael@gmail.com>
2021-12-08 16:04:13 -0800

    add name field to conversation model

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -2,7 +2,14 @@ const { Op } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
 
-const Conversation = db.define("conversation", {});
+const Conversation = db.define("conversation", {
+  // users should be allowed to name their group chats
+  // but the name will be null if it's just a regular conversation
+  name: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  }
+});
 
 // find conversation given two user Ids
 
